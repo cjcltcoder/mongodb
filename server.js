1257df840a556b03baf3f26b6129cfa8b0b4f1ce
Carlos <carlosj.corprez@gmail.com>
2024-02-02 17:41:23 -0500

    Loading data from budget_data.json

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { start } = require('repl');
 const app = express();
 const port = 3000;
+const fs = require('fs');
+const path = require('path');
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
+ /*const budget = {
+   myBudget: [
     {
         title: 'Eat out',
         budget: 25
@@ -16,19 +18,40 @@ const budget = {
         budget: 375
     },
     {
-        title: 'Chicken',
+        title: 'Car',
         budget: 110
     },
     ]
-};
+};*/
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
-});
+    const dataFilePath = path.join(__dirname, 'budget_data.json');
+  
+    fs.readFile(dataFilePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading budget data:', err);
+        return res.status(500).send('Internal Server Error');
+      }
+  
+      try {
+        const budgetData = JSON.parse(data);
+        res.json(budgetData.myBudget);
+      } catch (error) {
+        console.error('Error parsing JSON data:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+  });
+  
+
+//app.get('/budget', (req, res) => {
+    //res.json(budget);
+//});
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
